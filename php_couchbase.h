432c2e5c4a9d338b72502bde7884ec743100d2bf
Sergey Avseyev <sergey.avseyev@gmail.com>
2017-04-10 16:27:14 +0300

    Append snapshot to version to avoid confusion
    Change-Id: I0ee84f1c451709a3de96d8fd6bd57b3b61df5315
    Reviewed-on: http://review.couchbase.org/76554
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Build Bot <build@couchbase.com>

--- php_couchbase.h
+++ php_couchbase.h
@@ -17,7 +17,7 @@
 #ifndef PHP_COUCHBASE_H_
 #define PHP_COUCHBASE_H_
 
-#define PHP_COUCHBASE_VERSION "2.3.1"
+#define PHP_COUCHBASE_VERSION "2.3.2snapshot"
 #define PHP_COUCHBASE_EXTNAME "couchbase"
 
 extern zend_module_entry couchbase_module_entry;
